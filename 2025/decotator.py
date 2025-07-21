1162b23b0bee5adfadea5ddb550cb22b20c9c158
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-07-21 20:09:47 +0530

    Create decotator.py

---
+++ 2025/decotator.py
@@ -0,0 +1,19 @@
+
+def name(data):
+        
+    def user():
+        
+        print("Start in the line from beginner")
+     
+        data()   
+        
+        print ("ending the line")
+        
+    return user
+    
+@name
+def myinfo():
+    print("I am back")
+
+
+myinfo()
