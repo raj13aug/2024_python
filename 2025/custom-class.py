0d6646c316ae6d3ecf719c18c05bd0afe8b30006
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2025-07-30 21:48:07 +0530

    30july

---
+++ 2025/custom-class.py
@@ -0,0 +1,13 @@
+class Tag():
+    
+    def __init__(self):
+        self.tags = {}
+        
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
+        
+cloud = Tag()
+cloud.add("python")
+cloud.add("python")
+
+print(cloud.tags)
